dfc471bbf61ef090668a2153bbc8ff7c42df977e
brownepres <barnabas.epres@gmail.com>
2025-01-08 21:40:46 +0100

    initialized roberta sentiment model

--- speech_analysis.R
+++ speech_analysis.R
@@ -1,6 +1,33 @@
 install.packages("languageserver")
+install.packages("readxl")
+install.packages("httr")
+library(httr)
+library(readxl)
 
-text <- "hello"
-num <- 2 + 2
-num <- as.character(num)
-num
+url <- "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment"
+headers <- add_headers(Authorization = "Bearer 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")
+
+get_roberta_score <- function(text){
+    text <- text
+    response <- POST(url, headers, body = list(inputs = text), encode = "json")
+    content <- content(response)
+    return (content)
+}
+
+#TODO: automatize file read in from the excel sheet
+speeches_info <- read_excel("trump_speech_analysis/speeches.xlsx")
+speeches_info <- as.data.frame(speeches_info)
+
+
+text1 <- readLines('/Users/barnabasepres/University/TDK/trump_speech_analysis/text_files/speech_1_"2024-06-06"_AZ.txt') 
+words <- strsplit(text1, "\\s+")[[1]]
+
+for (i in 1:(length(words)-1)){
+    print(get_roberta_score(words[i:i+1]))
+    break
+    #TODO: combine words in bigrams
+    #get normal output
+    #collect output in a dataframe (maybe a list of sentiment scores which will be part of a df later)
+}
+get_roberta_score("thank you")
+
