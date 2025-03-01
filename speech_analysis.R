5af5f76ff52cd4bfbe3b874c3365f13736818cff
brownepres <165069825+brownepres@users.noreply.github.com>
2025-03-01 12:46:41 +0100

    modeling

--- speech_analysis.R
+++ speech_analysis.R
@@ -4,11 +4,11 @@ install.packages("httr")
 library(httr)
 library(readxl)
 
-url <- "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment"
-headers <- add_headers(Authorization = "Bearer xx")
+url <- "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment" 
+headers <- add_headers(Authorization = "Bearer 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")
 
 get_roberta_score <- function(text){
-    text <- text
+    text <- text # nolint
     response <- POST(url, headers, body = list(inputs = text), encode = "json")
     content <- content(response)
     return (content)
@@ -19,7 +19,7 @@ speeches_info <- read_excel("trump_speech_analysis/speeches.xlsx")
 speeches_info <- as.data.frame(speeches_info)
 
 
-text1 <- readLines('/Users/barnabasepres/University/TDK/trump_speech_analysis/text_files/speech_1_"2024-06-06"_AZ.txt') 
+text1 <- readLines('/Users/barnabasepres/University/TDK/trump_speech_analysis/text_files/speech_1_"2024-06-06"_AZ.txt')  # nolint
 words <- strsplit(text1, "\\s+")[[1]]
 
 for (i in 1:(length(words)-1)){
@@ -30,4 +30,3 @@ for (i in 1:(length(words)-1)){
     #collect output in a dataframe (maybe a list of sentiment scores which will be part of a df later)
 }
 get_roberta_score("thank you")
-
